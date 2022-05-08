7138e501346a66a68c0ba66747615c1e0596f5fe
HaySsor <hayss007@gmail.com>
2022-05-08 16:33:08 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# modal-ezs-txpmvu
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/modal-ezs-txpmvu)
---
+++ index.js
@@ -0,0 +1,36 @@
+// Import stylesheets
+import './style.css';
+
+const buttonAccepted = document.querySelector('[data-button="Accepted"]')
+const buttonRejected = document.querySelector('[data-button="rejected"]')
+const buttonExit = document.querySelector('[data-button="exit"]')
+const challengeButton = document.querySelector('[data-button="challenge"]')
+const backGround = document.querySelector('[data-background]')
+
+function closeModal(){
+  const modalBox = document.querySelector('.modalbox')
+  modalBox.classList.add('accepted')
+}
+
+function openModal(){
+  const modalBox = document.querySelector('.modalbox')
+  modalBox.classList.remove('accepted')
+}
+
+function acceptedModal(){
+  closeModal()
+  const plane = document.querySelector('.plane')
+  plane.classList.add('fly')
+
+}
+
+buttonExit.addEventListener('click',closeModal)
+buttonRejected.addEventListener('click',closeModal)
+challengeButton.addEventListener('click',openModal)
+buttonAccepted.addEventListener('click',acceptedModal)
+backGround.addEventListener('click',(e)=>{
+    if(e.target.matches('.background')){
+      closeModal()
+    }
+})
+
